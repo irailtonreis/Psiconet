c707f2d66c5372420e9820f9accae7f197ec07c6
irailtonreis <irailton.oliveira91@gmail.com>
2019-08-30 18:40:14 -0300

    Adicionando rota

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/index', function () {
+    return view('index');
+});
+
+Route::get('/cadastroCliente', function () {
+    return view('cadastroCliente');
+});
+
+Route::get('/cadastroPsicologo', function () {
+    return view('cadastroPsicologo');
+});
+
+Route::get('/sejaPsicologo', function () {
+    return view('sejaPsicologo');
+});
+
+Route::get('/psicologoLogado', function () {
+    return view('psicologoLogado');
+});
+
+Route::get('/clienteLogado', function () {
+    return view('clienteLogado');
+});
+
+
+
+
+
