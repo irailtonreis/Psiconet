01a671a7c9bba96c0ae2a50a9a0c87da4eefdcd1
marceloferracini <marceloferracini@gmail.com>
2019-09-26 11:51:01 -0300

    Updates pages Marcelo

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -142,6 +142,249 @@
 					</div>
 		</div>
 	</section>
+	
+
+
+	
+	{{-- <div class="container">
+			<div class="row">
+					<div class="col-lg-12">
+						<div class="sec-title mt-5 mb-5 py-3">
+							<h2>O que dizem sobre a Psiconet<span class="sec-title-border"><span></span><span></span><span></span></span>
+							</h2>
+							<p>Opinião de alguns pacientes, sobre que acham dos serviços da Psiconet e nossos psicólogos.</p>
+						</div>
+					</div>
+				</div>
+		<div class="crousel-container">
+			<div id="carousel">
+					<div class="hideLeft mt-5">
+						<div class="testimonial">
+							<img src="{{url('img/author1.jpg')}}">
+						</div>
+					 
+				   </div>
+			 
+				   <div class="prevLeftSecond">
+						<div class="testimonial">
+								<img src="{{url('img/author2.jpg')}}">
+							</div>
+				   </div>
+			 
+				   <div class="prev">
+						<div class="testimonial">
+								<img src="{{url('img/author3.jpg')}}">
+							</div>
+				   </div>
+			 
+				   <div class="selected">
+						<div class="testimonial">
+								<img src="{{url('img/author4.jpg')}}">
+							</div>
+				   </div>
+			 
+				   <div class="next">
+						<div class="testimonial">
+								<img src="{{url('img/author5.jpg')}}">
+							</div>
+				   </div>
+			 
+				   <div class="nextRightSecond">
+						<div class="testimonial">
+								<img src="{{url('img/author6.jpg')}}">
+							</div>
+				   </div>
+			 
+				   <div class="hideRight">
+						<div class="testimonial">
+								<img src="{{url('img/author7.jpg')}}">
+							</div>
+				   </div>
+				 </div>
+			 
+				 <div class="buttons text-center">
+				   <button id="prev">Prev</button>
+				   <button id="next">Next</button>
+				 </div>
+				</div>
+	</div>  --}}
+
+	{{-- <div class="container custom-duvidas" id="duvidas">
+		<div class="text-center py-5">
+		<h2>Perguntas Frequentes</h2>
+		<h4>Veja porque o Psiconet é a melhor opção para você</h4>
+		</div>
+    <div class="row py-3 px-3">
+        <a class="alert-link" data-toggle="collapse" href="#collapseDuvidas" role="button" aria-expanded="false" aria-controls="collapseDuvidas">
+        O que é a Psiconet?
+        </a>
+		<div data-toggle="collapse" data-target="#collapseDuvidas" aria-expanded="false" aria-controls="collapseDuvidas">
+		</div>
+	
+        <div class="collapse" id="collapseDuvidas">
+			<div class="px-3 text-left">
+				<p>
+				A Psiconet foi criada para conectar você a psicólogos licenciados e experientes através de sessões de vídeo de forma simples, confidencial e muito mais acessível.
+				</p>
+			</div>
+        </div>
+    </div>
+
+	<div class="row px-3">
+        <a class="alert-link" data-toggle="collapse" href="#collapseDuvidas1" role="button" aria-expanded="false" aria-controls="collapseDuvidas1">
+        Como a Psiconet pode melhorar minha vida?
+        </a>
+		<div data-toggle="collapse" data-target="#collapseDuvidas1" aria-expanded="false" aria-controls="collapseDuvidas1">
+		</div>
+	
+        <div class="collapse" id="collapseDuvidas1">
+			<div class="px-3 text-left">
+				<p>
+				Temos mais de 1.500 psicólogos licenciados para ajudar você. A grande maioria desses profissionais é experiente em diversas áreas. Já ajudamos pessoas pelos seguintes motivos: 
+				</p>
+				<p>Descontrole emocional</p>
+				<p>Distúrbio alimentar - Extrema magreza</p>
+				<p>Doença psicológica</p>
+				<p>Fibromialgia</p>
+				<p>Pensamento obsessivo</p>
+				<p>Estresse pós-traumático </p>
+				<p>Problemas de relacionamento </p>
+				<p>Problemas no trabalho</p>
+			</div>
+        </div>
+    </div>
+
+	   <div class="row py-3 px-3">
+        <a class="alert-link" data-toggle="collapse" href="#collapseDuvidas2" role="button" aria-expanded="false" aria-controls="collapseDuvidas2">
+        A terapia online realmente funciona?
+        </a>
+		<div data-toggle="collapse" data-target="#collapseDuvidas2" aria-expanded="false" aria-controls="collapseDuvidas2">
+		</div>
+	
+        <div class="collapse" id="collapseDuvidas2">
+			<div class="px-3 text-left">
+				<p>
+				Segundo diversos estudos realizados na última década, a terapia online tem eficácia comprovada, assim como a presencial. Além disso, para assegurar a efetividade do tratamento, a Psiconet seleciona cuidadosamente seus terapeutas, sendo todos licenciados e credenciados pelo Conselho de Psicologia. 
+				</p>
+			</div>
+        </div>
+    </div>
+
+	   <div class="row py-3 px-3">
+        <a class="alert-link" data-toggle="collapse" href="#collapseDuvidas3" role="button" aria-expanded="false" aria-controls="collapseDuvidas3">
+        Terapia online é confiável?
+        </a>
+		<div data-toggle="collapse" data-target="#collapseDuvidas3" aria-expanded="false" aria-controls="collapseDuvidas3">
+		</div>
+	
+        <div class="collapse" id="collapseDuvidas3">
+			<div class="px-3 text-left">
+				<p>
+				Sim, segundo diversos estudos realizados na última década, a terapia online tem eficácia comprovada, assim como a presencial.
+				</p>
+			</div>
+        </div>
+    </div>
+
+		   <div class="row py-3 px-3">
+        <a class="alert-link" data-toggle="collapse" href="#collapseDuvidas4" role="button" aria-expanded="false" aria-controls="collapseDuvidas4">
+        Como Funciona?
+        </a>
+		<div data-toggle="collapse" data-target="#collapseDuvidas4" aria-expanded="false" aria-controls="collapseDuvidas4">
+		</div>
+	
+        <div class="collapse" id="collapseDuvidas4">
+			<div class="px-3 text-left">
+				<p>
+				Em seu primeiro contato, você selecionará o psicólogo com quem mais se identificar. Depois, o profissional selecionado responderá algumas perguntas sobre os motivos que fizeram você a buscar ajuda. Por fim, você será direcionado para selecionar o plano de assinatura que preferir.
+				</p>
+			</div>
+        </div>
+    </div>
+
+	   <div class="row py-3 px-3">
+        <a class="alert-link" data-toggle="collapse" href="#collapseDuvidas5" role="button" aria-expanded="false" aria-controls="collapseDuvidas5">
+        Os psicólogos que atendem na Psiconet são licenciados?
+        </a>
+		<div data-toggle="collapse" data-target="#collapseDuvidas5" aria-expanded="false" aria-controls="collapseDuvidas5">
+		</div>
+	
+        <div class="collapse" id="collapseDuvidas5">
+			<div class="px-3 text-left">
+				<p>
+				Com certeza! Na Psiconet selecionamos criteriosamente os psicólogos que se cadastram para atender conosco. Durante o processo de seleção levamos em consideração o tempo de experiência, os cursos, a faculdade e pós graduação, experiência clínica entre outros detalhes técnicos.
+				Todos os psicólogos que atuam a Psiconet já ajudaram milhares de pessoas em nossa plataforma e poderão ajudar você também. 
+				</p>
+			</div>
+        </div>
+    </div>
+
+	  <div class="row py-3 px-3">
+        <a class="alert-link" data-toggle="collapse" href="#collapseDuvidas6" role="button" aria-expanded="false" aria-controls="collapseDuvidas6">
+        Eu sempre ficarei em contato com o mesmo psicólogo?
+        </a>
+		<div data-toggle="collapse" data-target="#collapseDuvidas6" aria-expanded="false" aria-controls="collapseDuvidas6">
+		</div>
+	
+        <div class="collapse" id="collapseDuvidas6">
+			<div class="px-3 text-left">
+				<p>
+				Sim, você sempre permanecerá com o mesmo psicólogo selecionado. Caso não se adapte ao psicólogo, você também pode solicitar a troca diretamente pelo “Suporte” no nosso site. 
+				</p>
+			</div>
+        </div>
+    </div>
+
+	  <div class="row py-3 px-3">
+        <a class="alert-link" data-toggle="collapse" href="#collapseDuvidas7" role="button" aria-expanded="false" aria-controls="collapseDuvidas7">
+        Posso trocar de psicólogo?
+        </a>
+		<div data-toggle="collapse" data-target="#collapseDuvidas7" aria-expanded="false" aria-controls="collapseDuvidas7">
+		</div>
+	
+        <div class="collapse" id="collapseDuvidas7">
+			<div class="px-3 text-left">
+				<p>
+				Sim. Caso não tenha se adaptado com o seu psicólogo, você pode solicitar outro profissional no “Suporte” dentro do nosso site. 
+				</p>
+			</div>
+        </div>
+    </div>
+
+	  <div class="row py-3 px-3">
+        <a class="alert-link" data-toggle="collapse" href="#collapseDuvidas8" role="button" aria-expanded="false" aria-controls="collapseDuvidas8">
+        Tenho privacidade nas minhas conversas?
+        </a>
+		<div data-toggle="collapse" data-target="#collapseDuvidas8" aria-expanded="false" aria-controls="collapseDuvidas8">
+		</div>
+	
+        <div class="collapse" id="collapseDuvidas8">
+			<div class="px-3 text-left">
+				<p>
+				Sim sua segurança e privacidade é incontestável. Por isso, nossa plataforma é 100% criptografada, a fim de garantir que apenas você e seu terapeuta tenham acesso ao conteúdo das sessões.
+				Além disso, a terapia online segue o Código de Ética da Psicologia, que exige confidencialidade e sigilo nas informações reveladas pelo paciente. 
+				</p>
+			</div>
+        </div>
+    </div>
+
+	<div class="row py-3 px-3">
+        <a class="alert-link" data-toggle="collapse" href="#collapseDuvidas9" role="button" aria-expanded="false" aria-controls="collapseDuvidas9">
+        Tenho menos de 18 anos, posso usar a Psiconet?
+        </a>
+		<div data-toggle="collapse" data-target="#collapseDuvidas9" aria-expanded="false" aria-controls="collapseDuvidas9">
+		</div>
+	
+        <div class="collapse" id="collapseDuvidas9">
+			<div class="px-3 mt-4 text-left">
+				<p>
+				Infelizmente não. Apenas maiores de 18 anos podem utilizar os serviços da Psiconet.
+				</p>
+			</div>
+        </div>		
+	</div> --}}
+	
+	<!-- Section Psicólogos inicio-->
 	<!-- team section start -->
 	<section class="team-area ptb-90" id="team">
 		<div class="container">
@@ -163,10 +406,10 @@
 							<div class="team-member-icon">
 								<div class="display-table">
 									<div class="display-tablecell">
-										<a href="#"><i class="icofont icofont-social-facebook"></i></a>
-										<a href="#"><i class="icofont icofont-social-twitter"></i></a>
-										<a href="#"><i class="icofont icofont-brand-linkedin"></i></a>
-										<a href="#"><i class="icofont icofont-social-pinterest"></i></a>
+										<a href="http://facebook.com"><i class="icofont icofont-social-facebook"></i></a>
+										<a href="http://twitter.com"><i class="icofont icofont-social-twitter"></i></a>
+										<a href="http://linkedin.com"><i class="icofont icofont-brand-linkedin"></i></a>
+										<a href="https://br.pinterest.com/"><i class="icofont icofont-social-pinterest"></i></a>
 									</div>
 								</div>
 							</div>
@@ -186,10 +429,10 @@
 							<div class="team-member-icon">
 								<div class="display-table">
 									<div class="display-tablecell">
-										<a href="#"><i class="icofont icofont-social-facebook"></i></a>
-										<a href="#"><i class="icofont icofont-social-twitter"></i></a>
-										<a href="#"><i class="icofont icofont-brand-linkedin"></i></a>
-										<a href="#"><i class="icofont icofont-social-pinterest"></i></a>
+										<a href="http://facebook.com"><i class="icofont icofont-social-facebook"></i></a>
+										<a href="http://twitter.com"><i class="icofont icofont-social-twitter"></i></a>
+										<a href="#http://linkedin.com"><i class="icofont icofont-brand-linkedin"></i></a>
+										<a href="https://br.pinterest.com/"><i class="icofont icofont-social-pinterest"></i></a>
 									</div>
 								</div>
 							</div>
@@ -209,10 +452,10 @@
 							<div class="team-member-icon">
 								<div class="display-table">
 									<div class="display-tablecell">
-										<a ref="#"><i class="icofont icofont-social-facebook"></i></a>
-										<a href="#"><i class="icofont icofont-social-twitter"></i></a>
-										<a href="#"><i class="icofont icofont-brand-linkedin"></i></a>
-										<a href="#"><i class="icofont icofont-social-pinterest"></i></a>
+										<a ref="http://facebook.com"><i class="icofont icofont-social-facebook"></i></a>
+										<a href="http://twitter.com"><i class="icofont icofont-social-twitter"></i></a>
+										<a href="http://linkedin.com"><i class="icofont icofont-brand-linkedin"></i></a>
+										<a href="https://br.pinterest.com/"><i class="icofont icofont-social-pinterest"></i></a>
 									</div>
 								</div>
 							</div>
@@ -232,10 +475,10 @@
 							<div class="team-member-icon">
 								<div class="display-table">
 									<div class="display-tablecell">
-										<a href="#"><i class="icofont icofont-social-facebook"></i></a>
-										<a href="#"><i class="icofont icofont-social-twitter"></i></a>
-										<a href="#"><i class="icofont icofont-brand-linkedin"></i></a>
-										<a href="#"><i class="icofont icofont-social-pinterest"></i></a>
+										<a href="http://facebook.com"><i class="icofont icofont-social-facebook"></i></a>
+										<a href="http://twitter.com"><i class="icofont icofont-social-twitter"></i></a>
+										<a href="http://linkedin.com"><i class="icofont icofont-brand-linkedin"></i></a>
+										<a href="https://br.pinterest.com/"><i class="icofont icofont-social-pinterest"></i></a>
 									</div>
 								</div>
 							</div>
